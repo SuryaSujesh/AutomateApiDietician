342c19ad80b684768d1117595c07df9aa8e3e882
sk <ksurya2008@gmail.com>
2023-12-11 20:00:30 -0500

    added

--- src/test/java/pojo/Pojo.java
+++ src/test/java/pojo/Pojo.java
@@ -1,5 +1,32 @@
 package pojo;
 
 public class Pojo {
+	
+	public static String patientInfo= "{\n"
+			+ "\n"
+			+ "\"FirstName\": \"Fred\",\n"
+			+ "\"LastName\": \"Lip\",\n"
+			+ "\"ContactNumber\": \"9978654322\",\n"
+			+ "\"Email\": \"fredlip@gmail.com\",\n"
+			+ "\"Allergy\": \"Cashew\",\n"
+			+ "\"FoodCategory\": \"Vegan\",\n"
+			+ "\"DateOfBirth\": \"1971-06-09\"\n"
+			+ "\n"
+			+ "}";
+	
+	
+	public static String updateInfo= "{\n"
+			+ "\n"
+			+ "\"FirstName\": \"Fred\",\n"
+			+ "\"LastName\": \"Lip\",\n"
+			+ "\"ContactNumber\": \"9978654321\",\n"
+			+ "\"Email\": \"fredlip@gmail.com\",\n"
+			+ "\"Allergy\": \"Cashew\",\n"
+			+ "\"FoodCategory\": \"Vegan\",\n"
+			+ "\"DateOfBirth\": \"1970-06-09\"\n"
+			+ "\n"
+			+ "}";
+	
+
 
 }
